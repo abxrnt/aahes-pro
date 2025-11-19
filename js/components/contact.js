10aa2fad1faaf049a4523ebe3212a887c261be54
abxrnt <145489153+abxrnt@users.noreply.github.com>
2025-11-19 16:24:56 +0530

    Create contact.js

---
+++ js/components/contact.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } 
+  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOVl1hdmprJXpgdN_sNTO3FsNePD_zNZY",
+  authDomain: "aahes-cee-cutoffs.firebaseapp.com",
+  databaseURL: "https://aahes-cee-cutoffs-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aahes-cee-cutoffs",
+  storageBucket: "aahes-cee-cutoffs.firebasestorage.app",
+  messagingSenderId: "706066626405",
+  appId: "1:706066626405:web:0d254ab82beb288108938c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Wait for content to load (important for components)
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const form = document.getElementById("contactForm");
+  if (!form) return; // safety check
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const phn = phoneInput.value.trim();
+    const msg = msgInput.value.trim();
+
+    await addDoc(collection(db, "noti"), {
+      name,
+      email,
+      phn,
+      msg,
+      time: serverTimestamp()
+    });
+
+    alert("Message sent!");
+    form.reset();
+  });
+
+});
